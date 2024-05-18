7b7031d60d880bdd750ac68a3bd71399007c8963
Kikuchi Ken <ken@nuasa.org>
2024-05-18 21:52:20 +0900

    ビューポートサイズをパラメータで指定できるよう修正

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const puppeteer = require('puppeteer');
 const PUPPETEER_LAUNCH_OPTION = {
   args: ['--lang=ja']
 };
+const DEFAULT_WIDTH = 1920;
+const DEFAULT_HEIGHT = 1080;
+const DEFAULT_SCALE = 1;
 
-function checkRequest(req) {
+function getParams(req) {
   if(req.method !== "POST") {
     const error = new Error("only POST method are accepted");
     error.code = 405;
@@ -16,16 +19,47 @@ function checkRequest(req) {
     error.code = 400;
     throw error;
   }
+
+  const width = req.body.width ? parseInt(req.body.width) : DEFAULT_WIDTH;
+  const height = req.body.height ? parseInt(req.body.height) : DEFAULT_HEIGHT;
+  const scale = req.body.scale ? parseInt(req.body.scale) : DEFAULT_SCALE;
+
+  return [
+    req.body.url,
+    req.body.html,
+    width,
+    height,
+    scale,
+  ];
+}
+
+/*
+  
+*/
+async function getNewPage(browser, width = DEFAULT_WIDTH, height = DEFAULT_HEIGHT, scale = DEFAULT_SCALE) {
+  const page = await browser.newPage();
+  await page.setViewport({
+    width: width,
+    height: height,
+    deviceScaleFactor: scale,
+  });
+  await page.setExtraHTTPHeaders({
+    'Accept-Language': 'ja-JP'
+  });
+  return page;
 }
 
 /*
   url: the URL for rendering
   type: "pdf" | "png"
+  width: viewport width (ignored when type == "pdf")
+  height: viewport height (ignored when type == "pdf")
+  scale: device scale factor (ignored when type == "pdf")
 */
-async function renderFromUrl(url, type = "pdf") {
+async function renderFromUrl(url, type = "pdf", width = DEFAULT_WIDTH, height = DEFAULT_HEIGHT, scale = DEFAULT_SCALE) {
   try {
     const browser = await puppeteer.launch(PUPPETEER_LAUNCH_OPTION);
-    const page = await browser.newPage();
+    const page = await getNewPage(browser, width, height, scale);
     await page.goto(url, {
       waitUntil: "networkidle0"
     });
@@ -42,11 +76,14 @@ async function renderFromUrl(url, type = "pdf") {
 /*
   html: HTML for rendering
   type: "pdf" | "png"
+  width: viewport width (ignored when type == "pdf")
+  height: viewport height (ignored when type == "pdf")
+  scale: device scale factor (ignored when type == "pdf")
 */
-async function renderFromHtml(html, type = "pdf") {
+async function renderFromHtml(html, type = "pdf", width = DEFAULT_WIDTH, height = DEFAULT_HEIGHT, scale = DEFAULT_SCALE) {
   try {
     const browser = await puppeteer.launch(PUPPETEER_LAUNCH_OPTION);
-    const page = await browser.newPage();
+    const page = await getNewPage(browser, width, height, scale);
     await page.setContent(html);
     const buffer = type === "png" ? await page.screenshot() : await page.pdf();
     await browser.close();
@@ -61,12 +98,16 @@ async function renderFromHtml(html, type = "pdf") {
 /*
   URLもしくはHTMLで指定されたWebページのスクリーンショットを作成する
   method: POST
-  params: 'url' もしくは 'html' のいずれかが必須
+  @url : 取得するページのURL
+  @html : 取得するページのHTML
+  @width : ビューポートの幅 (optional, default: 1920)
+  @height : ビューポートの高さ (optional, default: 1080)
+  @scale : デバイススケールファクター (optional, default: 1)
 */
 functions.http('screenshot', async (req, res) => {
   try {
-    checkRequest(req);
-    const screenshot = req.body.url ? await renderFromUrl(req.body.url, "png") : await renderFromHtml(req.body.html, "png");
+    const [url, html, width, height, scale] = getParams(req);
+    const screenshot = url ? await renderFromUrl(url, "png", width, height, scale) : await renderFromHtml(html, "png", width, height, scale);
     res.type('png').send(screenshot);
   }
   catch(err) {
@@ -78,12 +119,16 @@ functions.http('screenshot', async (req, res) => {
 /*
   URLもしくはHTMLで指定されたWebページをPDFにする
   method: POST
-  params: 'url' もしくは 'html' のいずれかが必須
+  @url : 取得するページのURL
+  @html : 取得するページのHTML
+  @width : ビューポートの幅 (optional, default: 1920)
+  @height : ビューポートの高さ (optional, default: 1080)
+  @scale : デバイススケールファクター (optional, default: 1)
 */
 functions.http('make_pdf', async (req, res) => {
   try {
-    checkRequest(req);
-    const pdf = req.body.url ? await renderFromUrl(req.body.url, "pdf") : await renderFromHtml(req.body.html, "pdf");
+    const [url, html, width, height, scale] = getParams(req);
+    const pdf = url ? await renderFromUrl(url, "pdf", width, height, scale) : await renderFromHtml(html, "pdf", width, height, scale);
     res.type('pdf').send(pdf);
   }
   catch(err) {
