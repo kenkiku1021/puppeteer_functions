3fc474b412ef4d9c79158a100746002ad1d8a314
Kikuchi Ken <kikuchi@hamajima.co.jp>
2024-06-26 15:41:22 +0900

    Cloud Storageを使ったPDF生成用Cloud Functionを追加

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const functions = require('@google-cloud/functions-framework');
 const puppeteer = require('puppeteer');
 const { execFile } = require('child_process');
+const {Storage} = require('@google-cloud/storage');
+const crypto = require("crypto");
 
 const PUPPETEER_LAUNCH_OPTION = {
   args: [
@@ -20,6 +22,7 @@ const PUPPETEER_PDF_OPTION = {
 const DEFAULT_WIDTH = 1920;
 const DEFAULT_HEIGHT = 1080;
 const DEFAULT_SCALE = 1;
+const GS_EXPIRATION_MS = 300 * 1000; // 300秒
 
 async function init() {
   setFontConfig();
@@ -204,3 +207,47 @@ functions.http('make_pdf', async (req, res) => {
   }
 });
 
+/*
+  URLもしくはHTMLで指定されたWebページをPDFにし、Cloud Storageに保存する
+  method: POST
+  @url : 取得するページのURL
+  @html : 取得するページのHTML
+  戻り値 : JSON {url:, expires:}
+*/
+functions.http('make_pdf_gs', async (req, res) => {
+  try {
+    res.set('Access-Control-Allow-Origin', '*');
+    if (req.method === 'OPTIONS') {
+      // Send response to OPTIONS requests
+      res.set('Access-Control-Allow-Methods', 'POST');
+      res.set('Access-Control-Allow-Headers', 'Content-Type');
+      res.set('Access-Control-Max-Age', '3600');
+      res.status(204).send('');
+    }
+    else {
+      const [url, html, width, height, scale] = getParams(req);
+      const pdf = url ? await renderFromUrl(url, "pdf", width, height, scale) : await renderFromHtml(html, "pdf", width, height, scale);
+      const bucketName = process.env.BUCKET_NAME;
+      const storage = new Storage();
+      const bucket = storage.bucket(bucketName);
+      const filename = `${crypto.randomUUID()}.pdf`;
+      const file = bucket.file(filename);
+      await file.save(pdf);
+      const expires = Date.now() + GS_EXPIRATION_MS;
+      const expiresAt = new Date(expires);
+      const [signedUrl] = await file.getSignedUrl({
+        action: "read",
+        expires: expires,
+      });
+      const result = {
+        url: signedUrl,
+        expires: expiresAt.toJSON(),
+      };
+      res.json(result);
+    }
+  }
+  catch(err) {
+    console.error(err);
+    res.status(err.code || 500).send(err);
+  }
+});
